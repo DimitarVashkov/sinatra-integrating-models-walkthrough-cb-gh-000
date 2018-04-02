1a3a96c0da0ecceb33c2a0962f25c6858fd3c235
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-02 22:26:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    text = TextAnalyzer.new(text_from_user)
+    @words = text.count_of_words
+    @vowels = text.count_of_vowels
+    @cons = text.count_of_consonants
+    letters = text.most_used_letter
+    @freq_letter = letters.key
+    @freq_letter_count = letters.value
     erb :results
   end
 end
